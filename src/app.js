9ff0d67e78e1bf16b80c399a51cbb3da1f882f48
Ghazal <404ghazal@gmail.com>
2025-10-07 21:50:44 +0530

    handles diffrerent routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,35 @@ const express = require("express");
 const app = express();
 const port = 7777;
 
-app.use("/hello", (req,res) => {
-    // console.log("got a request on / route");
-    res.send("Hello hello hello!")
-   
+
+
+app.get("/profile/:id", (req, res ) => {
+    console.log(req.params.id);
+    res.send({
+        name: "Ghazala Shameem",
+        email: "email353example.com"
+    });
 })
- 
+
+app.post("/profile", (req, res) => {
+    res.send({
+        name: "This is post request",
+        email: "postexample.com"})
+})
+
+app.delete("/profile", (req, res) => {
+    res.send({
+        name: "This is delete request",
+        email: "deleteexample.com"})
+})
+
+
+
 
 app.use("/test", (req, res) => {
     res.send("<h1>I am heading 1 from a server to test can we send html data also</h1>")
 })
+
 
 
 app.listen(port, () => {
